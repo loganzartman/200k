55bf9272b96438c9ea15edda3b126b5d3685284a
Nondefault <spam@nondefault.net>
2016-07-15 04:51:17 -0500

    Added features, improved performance

--- flow.js
+++ flow.js
@@ -1,20 +1,22 @@
 var Flow = {
-	w: Math.floor(window.innerWidth/2),
-	h: Math.floor(window.innerHeight/2),
-	N: 128000,
+	w: Math.min(960, Math.floor(window.innerWidth/1)),
+	h: Math.min(700, Math.floor(window.innerHeight/1)),
+	N: 200000,
 	nProps: 5,
 	t0: Date.now(),
 	insertIdx: 0,
 
 	settings: {
-		gravity: 0.5,
+		gravity: 0.65,
+		centerGravity: 0.0,
+		wind: 0.35,
 		friction: 0.0
 	},
 
 	init: function() {
 		document.title = (""+Flow.N).replace(/(\d{3})(?!$)/g,"$1,")+" particles";
 		Flow.particles = new Float32Array(Flow.N * Flow.nProps);
-		Flow.pColors = new Uint8Array(Flow.N * 4);
+		Flow.pColors = new Int32Array(Flow.N);
 		for (var i=0,j=Flow.N; i<j; i++) {
 			var val = i/j;
 			var sector = ~~(val*6);
@@ -46,27 +48,24 @@ var Flow = {
 					b = z;
 				break;
 			}
-			Flow.pColors[i*4] = r;
-			Flow.pColors[i*4+1] = g;
-			Flow.pColors[i*4+2] = b;
-			Flow.pColors[i*4+3] = a;
+			Flow.pColors[i] = (a<<24)|(b<<16)|(g<<8)|(r);
 		}
 
 		Flow.output = document.getElementById("info");
 		Flow.canvas = document.getElementById("display");
-		Flow.canvas.width = Flow.w;
-		Flow.canvas.height = Flow.h;
+		Flow.canvas.width = Flow.canvas.style.width = Flow.w;
+		Flow.canvas.height = Flow.canvas.style.height = Flow.h;
 
 		document.addEventListener("mousemove", function(event){
-			Mouse.px = Mouse.x;
-			Mouse.py = Mouse.y;
-			Mouse.x = Math.min(Flow.w, Math.max(0, event.pageX * (Flow.w/window.innerWidth)));
-			Mouse.y = Math.min(Flow.h, Math.max(0, event.pageY * (Flow.h/window.innerHeight)));
+			Mouse.x = Math.min(Flow.w, Math.max(0, event.pageX - Flow.canvas.offsetLeft));
+			Mouse.y = Math.min(Flow.h, Math.max(0, event.pageY - Flow.canvas.offsetTop));
 		}, false);
 
 		var gui = new dat.GUI();
 		var phys = gui.addFolder("Physics");
-		phys.add(Flow.settings, "gravity").min(-2).max(2).step(0.05);
+		phys.add(Flow.settings, "gravity").min(0).max(1).step(0.05);
+		phys.add(Flow.settings, "centerGravity").min(0).max(1).step(0.05);
+		phys.add(Flow.settings, "wind").min(0).max(1).step(0.05);
 		phys.add(Flow.settings, "friction").min(0).max(1).step(0.05);
 
 		Flow.ctx = Flow.canvas.getContext("2d");
@@ -99,6 +98,7 @@ var Flow = {
 		dy /= steps;
 		var posX = Mouse.px,
 			posY = Mouse.py;
+		if (len > 0)
 		for (var j=0; j<steps; j++) {
 			for (var k=0; k<pps; k++) {
 				Flow.insertIdx = (Flow.insertIdx+1)%Flow.N;
@@ -113,16 +113,23 @@ var Flow = {
 				Flow.particles[idx+4] = Math.sin(dir2)*len2 + Math.random()*2 - 1;
 			}
 		}
+		Mouse.px = Mouse.x;
+		Mouse.py = Mouse.y;
 
-		var bcol = Flow.color(0,0,0,255);
+		//clear
+		var bcol = Flow.color(13,11,10,255);
 		for (var i=0,j=Flow.w*Flow.h; i<j; i++) {
 			Flow.buffer32[i] = bcol;
 		}
 
+		//step
 		var cx = Flow.w*0.5, cy = Flow.h*0.5;
 		var particles = Flow.particles;
 		var gravity = Flow.settings.gravity;
+		var cgravity = Flow.settings.centerGravity;
+		var wind = Flow.settings.wind;
 		var friction = 1-Flow.settings.friction;
+		var sin = FastMath.sin;
 		for (i=0,j=particles.length; i<j; i+=Flow.nProps) {
 			if (particles[i+0] === 0) continue;
 			particles[i+1] += particles[i+3];
@@ -131,12 +138,24 @@ var Flow = {
 			var y = particles[i+2];
 
 			//gravity
-			var ox = x - cx, oy = y - cy;
-			var dist = Math.sqrt(ox*ox+oy*oy);
-			ox /= dist;
-			oy /= dist;
-			particles[i+3] = particles[i+3] * friction;
-			particles[i+4] = particles[i+4] * friction + gravity;
+			var gx=0, gy=0;
+			if (cgravity !== 0) {
+				gx = x - cx;
+				gy = y - cy;
+				var dist = Math.sqrt(gx*gx+gy*gy);
+				gx /= dist;
+				gy /= dist;
+			}
+			
+			//wind
+			var wx=0, wy=0;
+			if (wind !== 0) {
+				wx = (sin(y*0.012 + t*1.24) * 2.5 + sin(y*0.028 + t*6.25) * 1.0) * wind;
+				wy = sin(x*0.02 + t*1.24) * wind;
+			}
+
+			particles[i+3] = particles[i+3] * friction - gx*cgravity + wx;
+			particles[i+4] = particles[i+4] * friction + gravity - gy*cgravity + wy;
 
 			//bounds
 			if (x<0 || y<0 || x>Flow.w || y>Flow.h) {
@@ -145,19 +164,31 @@ var Flow = {
 			}
 
 			//draw
-			var cidx = ~~(i/Flow.nProps*4);
-			var r = Flow.pColors[cidx+0];
-			var g = Flow.pColors[cidx+1];
-			var b = Flow.pColors[cidx+2];
-			var a = Flow.pColors[cidx+3];
-
 			var idx = (~~y)*Flow.w+(~~x);
-			Flow.buffer32[idx] = (a<<24)|(b<<16)|(g<<8)|(r);
+			Flow.buffer32[idx] = Flow.pColors[~~(i/5)];
 		}
 		Flow.idata.data.set(Flow.buffer8);
 		Flow.ctx.putImageData(Flow.idata, 0, 0);
+		var tmp = Flow.occlusion;
+		Flow.occlusion = Flow.occlusionWrite;
+		Flow.occlusionWrite = tmp;
 
 		requestAnimationFrame(Flow.draw);
+	}
+};
+
+var FastMath = {
+	sin: function(x) {
+		x*=0.159155;
+		x-=~~x;
+		var xx=x*x;
+		var y=-6.87897;
+		y=y*xx+33.7755;
+		y=y*xx-72.5257;
+		y=y*xx+80.5874;
+		y=y*xx-41.2408;
+		y=y*xx+6.28077;
+		return x*y;
 	}
 };
 
